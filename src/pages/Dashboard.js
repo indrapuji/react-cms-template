c4618cf3d4719c166fb7ea46de907388c1393401
Indra Puji Novirwan <indrapuji@gmail.com>
2022-05-05 21:33:36 +0700

    edit readme

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
 
 const Dashboard = () => {
-   const location = useLocation();
-   console.log(location);
    return <div>Dashboard</div>;
 };
 
