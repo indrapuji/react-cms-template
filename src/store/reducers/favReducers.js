702c9d0eb7cd21baa7298a0f79f5f212051437df
Indra Puji Novirwan <indrapuji@gmail.com>
2022-05-07 16:16:00 +0700

    how to use redux

--- src/store/reducers/favReducers.js
+++ src/store/reducers/favReducers.js
@@ -9,4 +9,13 @@ function favReducers(state = initialState, action) {
     return state;
 }
 
+function userReducer(state = initialState, action) {
+    if (action.type === 'GETUSER') {
+        return { ...state, users: action.payload };
+    } else if (action.type === 'DETAILUSER') {
+        return { ...state, details: action.payload };
+    }
+    return state;
+}
+
 export default favReducers;
